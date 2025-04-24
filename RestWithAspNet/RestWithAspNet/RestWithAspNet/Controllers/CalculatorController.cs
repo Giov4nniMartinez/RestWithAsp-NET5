48978a83482f2d79928105a0b6983838e2d491c8
Giov4nniMartinez <44985097+Giov4nniMartinez@users.noreply.github.com>
2025-04-24 08:57:45 -0300

    Calculator
    Implementing Calculator Sum

---
+++ RestWithAspNet/RestWithAspNet/RestWithAspNet/Controllers/CalculatorController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+         private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{fristNumber}/{secondNumber}")]
+        public IActionResult Get( string fristNumber, string secondNumber)
+        {
+            if (IsNumeric(fristNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(fristNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());            
+            }
+            return BadRequest("Invalid Input ");
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber, out decimalValue)) 
+            { return decimalValue; }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.CultureInfo.InvariantCulture,
+                out number);
+
+            return isNumber;
+        }
+    }
+}
